bef6d7b59a229d9a812d5c5c07a6a1cd51891070
cvogel3434 <cvogel.a.m@gmail.com>
2022-12-05 10:59:48 -0600

    modified from update version to work in electron app, read to properly include in other projects

--- gui/js/modules/vg-titlebar.js
+++ gui/js/modules/vg-titlebar.js
@@ -59,53 +59,136 @@ var tbdom={ //menubar
     }
   }
 }
+
+var tdom = (rroot='')=>{
+  return{
+    [`#${tbdom.cont}.div`]:{
+      attributes:{},
+      children:{
+        [`#${tbdom.utils.groups.left}.div`]:{
+          attributes:{},
+          children:{
+            [`#${tbdom.more.cont}.img`]:{
+              attributes:{
+                class: "titlebar-button-action",
+                src: rroot + "assets/icons/menu-burger.png",
+                alt: "MORE",
+                title: "More"
+              },
+              children: null
+            },
+            [`#${tbdom.more.actions}.div`]:{
+              attributes:{
+                style: "display:none"
+              },
+              children:null
+            },
+            [`#${tbdom.utils.buttons.home}.img`]:{
+              attributes:{
+                class: "titlebar-button-action",
+                src: rroot + "assets/icons/home.png",
+                alt: "HOME",
+                title: "Home"
+              },
+              children: null
+            }
+          }
+        },
+        [`#${tbdom.title}.div`]:{
+          attributes:{
+          },
+          children:null
+        },
+        [`#${tbdom.utils.groups.right}.div`]:{
+          attributes:{},
+          children:{
+          [`#${tbdom.info.cont}.span`]:{
+            attributes:{},
+            children:{
+              [`#${tbdom.page.user}.img`]:{
+                attributes:{
+                  class: "titlebar-button-action",
+                  src: rroot + "assets/icons/user.png",
+                  alt: "USER",
+                  title: "Log Out"
+                },
+                children: null
+              },
+              [`#${tbdom.info.username}.span`]:{
+                attributes:{},
+                children:null
+              }
+            }
+          },
+          [`#${tbdom.login.cont}.div`]:{
+            attributes:{},
+            children:null
+          },
+            [`#${tbdom.utils.buttons.help}.img`]:{
+              attributes:{
+                class: "titlebar-button-action",
+                src: rroot + "assets/icons/info.png",
+                alt: "HELP",
+                title: "help"
+              },
+              children: null
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+
 var lastwinsize={
   x:window.innerWidth,
   y:window.innerHeight
 }
 
-document.getElementById(tbdom.window.close).addEventListener('click',(ele)=>{  // Close window
-  window.close(); 
-});
-document.getElementById(tbdom.window.mini).addEventListener('click',(ele)=>{  // Minimize window
-  ipcRenderer.send('view-minimize',document.getElementById(tbdom.title).innerText);
-});
 
-document.getElementById(tbdom.window.maxi).addEventListener('click',(ele)=>{  // Maximize window
-  if(screen.availWidth == window.innerWidth && screen.availHeight == window.innerHeight){
-    window.resizeTo(lastwinsize.x,lastwinsize.y);
-  }else{
-    lastwinsize.x = window.innerWidth;
-    lastwinsize.y = window.innerHeight;
-    window.resizeTo(screen.availWidth,screen.availHeight);
-  }
-});
+var SETUPtitlebar=(qacts={},macts={},closeeve=(ele)=>{window.close();})=>{
+  ADDqactions(qacts);
+  ADDmactions(macts);
 
-document.getElementById(tbdom.page.print).addEventListener('dblclick',(ele)=>{  // Print screen
-  ipcRenderer.send('print-screen',{file:document.getElementById(tbdom.title).innerText});
-});
+  document.getElementById(tbdom.window.close).addEventListener('click',closeeve);
 
-document.getElementById(tbdom.more.cont).addEventListener('click',(ele)=>{  // Toggle More Options menu
+  document.getElementById(tbdom.more.cont).addEventListener('click',(ele)=>{  // Toggle More Options menu
     let moreele = document.getElementById(tbdom.more.actions);
-      if($(moreele).is(":visible")){
-        $(moreele).hide();
-      }else{$(moreele).show();}
-
+    $(moreele).toggle();
+  });
+  document.getElementById(tbdom.page.print).addEventListener('dblclick',(ele)=>{  // Print screen
+    ipcRenderer.send('print-screen',{file:document.getElementById(tbdom.title).innerText});
+  });
+  document.getElementById(tbdom.window.maxi).addEventListener('click',(ele)=>{  // Maximize window
+    if(screen.availWidth == window.innerWidth && screen.availHeight == window.innerHeight){
+      window.resizeTo(lastwinsize.x,lastwinsize.y);
+    }else{
+      lastwinsize.x = window.innerWidth;
+      lastwinsize.y = window.innerHeight;
+      window.resizeTo(screen.availWidth,screen.availHeight);
+    }
+  });
+  document.getElementById(tbdom.window.mini).addEventListener('click',(ele)=>{  // Minimize window
+    ipcRenderer.send('view-minimize',document.getElementById(tbdom.title).innerText);
   });
 
+}
 
 /* ADD actions to title bar
     Function to add an array of elements to the more actions portion
     of the title bar.
 */
-var ADDmactions=(acts=[])=>{
+var ADDmactions=(acts={})=>{
+  acts=CREATEactionbuttons(acts)
   for(let x=0;x<acts.length;x++){
     acts[x].classList.add(tbdom.utils.buttons.action);
     document.getElementById(tbdom.more.actions).appendChild(acts[x]);
   }
 }
 
-var ADDqactions=(acts=[])=>{
+var ADDqactions=(acts={})=>{
+  acts=CREATEactionbuttons(acts)
   for(let x=0;x<acts.length;x++){
     acts[x].classList.add(tbdom.utils.buttons.action);
     document.getElementById(tbdom.utils.groups.left).insertBefore(acts[x],document.getElementById(tbdom.info.cont)); //refresh button
@@ -115,6 +198,11 @@ var CREATEactionbuttons=(acts)=>{
   let alist = [];
   for(let ma in acts){
     alist.push(document.createElement('img'));
+    if(ma.includes('spacer')){
+      acts[ma]={src:'./bin/repo/assets/icons/minus.png',title: ''}
+      alist[alist.length-1].classList.add('moretools-spacer');
+    }
+    alist[alist.length-1].classList.add(tbdom.utils.buttons.action);
     for(let as in acts[ma]){
       alist[alist.length-1][as]=acts[ma][as];
     }
@@ -131,7 +219,5 @@ var INITtitlebarONmain=(ipcMain)=>{
 module.exports={
   tbdom,
   INITtitlebarONmain,
-  ADDmactions,
-  ADDqactions,
-  CREATEactionbuttons
+  SETUPtitlebar
 }
